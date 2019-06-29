274b269bdd8d25ffee1722e78b1f1d78f2ceb384
lrxcy <lrxcywq@outlook.com>
2019-06-29 14:02:30 +0800

    ssl

--- util/deployer.sh
+++ util/deployer.sh
@@ -15,9 +15,9 @@ git config --global push.default matching
 git config --global user.email "${GitHubEMail}"
 git config --global user.name "${GitHubUser}"
 git remote add origin https://github.com/${GitHubKEY}/${GitHubRepo}.git
-git pull origin master
+git pull origin ssl
 rm -rf ./*
 cp -rf ../public/* ../_deploy/
 git add --all .
 git commit -m "Daily check of Chrome offline packages by Travis CI"
-git push --quiet --force origin HEAD:master
+git push --quiet --force origin HEAD:ssl
